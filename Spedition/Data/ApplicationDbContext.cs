9da17a5f500c816a1f45697f276428e282c57c8a
meef1k <m4t3ush3k@gmail.com>
2022-07-06 00:23:11 +0200

    Spedition done

--- Spedition/Data/ApplicationDbContext.cs
+++ Spedition/Data/ApplicationDbContext.cs
@@ -13,6 +13,6 @@ namespace Spedition.Data
         public DbSet<Truck> Truck { get; set; }
         public DbSet<Trailer> Trailer { get; set; }
         public DbSet<Driver> Driver { get; set; }
-        public DbSet<Speditions> Spedition { get; set; }
+        public DbSet<Speditions> Speditions { get; set; }
     }
 }
