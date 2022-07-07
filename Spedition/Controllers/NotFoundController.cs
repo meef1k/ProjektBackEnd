688defcf9529552b9a5f1fd249c2ac3075316207
meef1k <m4t3ush3k@gmail.com>
2022-07-07 13:51:04 +0200

    NotFound page added

---
+++ Spedition/Controllers/NotFoundController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Spedition.Controllers
+{
+    public class NotFoundController : Controller
+    {
+        [Route("Error/{statusCode}")]
+        public IActionResult HttpStatusCodeHandler(int statusCode)
+        {
+            switch (statusCode)
+            {
+                case 404:
+                    ViewBag.errorMessage = "Page not found";
+                    break;
+            }
+            return View("NotFound");
+        }
+    }
+}
