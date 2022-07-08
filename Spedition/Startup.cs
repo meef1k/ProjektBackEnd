23a30470b7431f535d2c250a4cb1cf2182c337fc
meef1k <m4t3ush3k@gmail.com>
2022-07-08 20:39:41 +0200

    Docker compose done

--- Spedition/Startup.cs
+++ Spedition/Startup.cs
@@ -40,8 +40,11 @@ namespace Spedition
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ApplicationDbContext dataContext)
         {
+            // migrate any database changes on startup (includes initial db creation)
+            dataContext.Database.Migrate();
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
